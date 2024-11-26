aa9287c3e2e85bc75e10f2b62de63c22dfaa055e
Adrian Jurisic <adrianjurisic1601@gmail.com>
2024-11-26 18:00:52 +0100

    Install required libraries, create main menu component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
+import {Container} from 'react-bootstrap';
+import { faHome } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { MainMenu } from '../MainMenu/MainMeni';
 
 function App() {
   return (
-    <div className="App">
-      Home
-    </div>
+    <Container className="App">
+      <FontAwesomeIcon icon = {faHome} /> Home
+    </Container>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'jquery/dist/jquery.js';
+import 'popper.js/dist/popper.js';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import '@fortawesome/fontawesome-free/css/fontawesome.css';
+import { MainMenu } from './components/MainMenu/MainMeni';
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <MainMenu></MainMenu>
     <App />
   </React.StrictMode>
 );
