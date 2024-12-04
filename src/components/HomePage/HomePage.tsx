a7a729938612860a5e8aefb1c65a5e9a3b7814f7
Adrian Jurisic <adrianjurisic1601@gmail.com>
2024-12-04 19:19:32 +0100

    Home page- category

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,14 +1,100 @@
 import React from 'react';
-import {Container} from 'react-bootstrap';
-import { faHome } from '@fortawesome/free-solid-svg-icons';
+import { Container, Card, Row, Col } from 'react-bootstrap';
+import { faListAlt } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import CategoryType from '../../types/CategoryType';
+import { Navigate, Link } from 'react-router-dom';
+import api, { ApiResponse } from '../../api/api';
+import ApiCategoryDto from '../../dtos/ApiCategoryDto';
 
-function HomePage() {
-  return (
-    <Container>
-      <FontAwesomeIcon icon = {faHome} /> Home
-    </Container>
-  );
+interface HomePageState {
+    isUserLoggedIn: boolean;
+    categories?: CategoryType[];
+}
+
+class HomePage extends React.Component {
+    state: HomePageState;
+
+    constructor(props: Readonly<{}>) {
+        super(props);
+
+        this.state = {
+            isUserLoggedIn: true,
+            categories: [],
+        };
+    }
+
+    componentDidMount() {
+        this.getCategories();
+    }
+
+    componentDidUpdate() {
+        this.getCategories();
+    }
+
+    private getCategories() {
+        api('api/category/?filter=parentCategoryId||$isnull ', 'get', {})
+        .then((res: ApiResponse) => {
+            if (res.status === "error" || res.status === "login") {
+                this.setLogginState(false);
+                return;
+            }
+
+            this.putCategoriesInState(res.data);
+        });
+    }
+
+    private putCategoriesInState(data?: ApiCategoryDto[]) {
+        const categories: CategoryType[] | undefined = data?.map(category => {
+            return {
+                categoryId: category.categoryId,
+                name: category.name,
+                items: [],
+            };
+        });
+
+        const newState = Object.assign(this.state, {
+            categories: categories,
+        });
+
+        this.setState(newState);
+    }
+
+    private setLogginState(isLoggedIn: boolean) {
+        const newState = Object.assign(this.state, {
+            isUserLoggedIn: isLoggedIn,
+        });
+
+        this.setState(newState);
+    }
+
+    render() {
+        if (this.state.isUserLoggedIn === false) {
+            return (
+                <Navigate to="/user/login" />
+            );
+        }
+
+        return (
+            <Container>
+                <Card>
+                    <Card.Body>
+                        <Card.Title>
+                            <FontAwesomeIcon icon={ faListAlt } /> Top level categories
+                        </Card.Title>
+
+                        <Row>
+                            
+                        </Row>
+                    </Card.Body>
+                </Card>
+            </Container>
+        );
+    }
+
+    private singleCategory(category: CategoryType) {
+        // TO DO...
+    }
 }
 
 export default HomePage;
