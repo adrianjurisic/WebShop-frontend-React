0b7fa94ca5257f93729e799ec83d1c10cd2580bd
Adrian Jurisic <adrianjurisic1601@gmail.com>
2024-12-16 16:54:29 +0100

    Mechanism for making order

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import CartType from "../../types/CartType";
 import api, {ApiResponse} from '../../api/api';
-import { Button, Modal, Nav, Table } from "react-bootstrap";
+import { Alert, Button, Form, Modal, Nav, Table } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCartArrowDown } from "@fortawesome/free-solid-svg-icons";
+import { faCartArrowDown, faMinusSquare } from "@fortawesome/free-solid-svg-icons";
 
 interface CartState {
     count: number;
     cart?: CartType;
     visible: boolean;
+    message: string;
+    cartMenuColor: string;
 }
 
 export default class Cart extends React.Component {
@@ -20,6 +22,8 @@ export default class Cart extends React.Component {
         this.state = {
             count: 0,
             visible: false,
+            message: '',
+            cartMenuColor: '#000000'
         }
     }
 
@@ -43,7 +47,15 @@ export default class Cart extends React.Component {
     }
 
     private setStateVisible(newState: boolean){
-        this.setState(Object.assign(this.state, {visible: newState}))
+        this.setState(Object.assign(this.state, {visible: newState}));
+    }
+
+    private setStateMessage(newMessage: string){
+        this.setState(Object.assign(this.state, {message: newMessage}));
+    }
+
+    private setCartMenuColor(newColor: string){
+        this.setState(Object.assign(this.state, {cartMenuColor: newColor}));
     }
 
     private showCart (){
@@ -51,6 +63,7 @@ export default class Cart extends React.Component {
     }
 
     private hideCart(){
+        this.setStateMessage('');
         this.setStateVisible(false);
     }
 
@@ -64,12 +77,13 @@ export default class Cart extends React.Component {
             }
 
             this.setStateCart(res.data);
-            console.log(res.data)
             if(!res.data.cartArticles.length){
                 this.setStateCount(0);
             } else {
                 this.setStateCount(res.data.cartArticles.length);
             }
+            this.setCartMenuColor('#DC0000');
+            setTimeout(()=> this.setCartMenuColor('#000000'), 2000);
         })
     }
 
@@ -83,12 +97,64 @@ export default class Cart extends React.Component {
         return sum;
     }
 
+    private sendCartUpdate(data: any){
+        api('/api/user/cart/', 'patch', data)
+        .then((res: ApiResponse) => {
+            if(res.status === 'error' || res.status === 'login'){
+                this.updateCart(); 
+                return;
+            }
+            this.setStateCart(res.data);
+            this.setStateCount(res.data.cartArticles.length);
+        });
+    }
+
+    private updateQuantity(event: React.ChangeEvent<HTMLInputElement>){
+        const articleId = Number(event.target.dataset.articleId);
+        const newQuantity = Number(event.target.value);
+        const updatedCart = { ...this.state.cart }; 
+        if(updatedCart.cartArticles !== undefined){
+            const cartArticle = updatedCart.cartArticles.find(item => item.article.articleId === articleId);
+            
+            if (cartArticle) {
+                cartArticle.quantity = newQuantity;
+            }
+        } 
+        this.setState({ cart: updatedCart });
+        this.sendCartUpdate({
+            articleId: articleId,
+            quantity: newQuantity,
+        });
+    }
+
+    private removeFromCart(articleId: number){
+        this.sendCartUpdate({
+            articleId: articleId,
+            quantity: 0,
+        })
+    }
+    
+    private makeOrder(){
+        api('/api/user/cart/makeOrder/', 'post', {})
+        .then((res: ApiResponse) => {
+            if(res.status === 'error' || res.status === 'login'){
+                this.setStateCount(0);
+                this.setStateCart(undefined);
+                return;
+            }
+
+            this.setStateMessage('Order sent! Thank You!');
+            this.setStateCart(undefined);
+            this.setStateCount(0);
+        });
+    }
+
     render() {
         const sum = this.calculateSum();
         return(
             <>
                 <Nav.Item> 
-                    <Nav.Link active={false} onClick={() => this.showCart()}>
+                    <Nav.Link active={false} onClick={() => this.showCart()} style={{color: this.state.cartMenuColor}}>
                         <FontAwesomeIcon icon={faCartArrowDown} /> ({this.state.count})
                     </Nav.Link>
                     
@@ -98,6 +164,8 @@ export default class Cart extends React.Component {
                         <Modal.Title>Your cart</Modal.Title>
                     </Modal.Header>
                     <Modal.Body>
+                        {this.state.cart?.cartArticles.length && this.state.cart.cartArticles.length > 0 ? (
+                        <>
                         <Table hover size="sm">
                             <thead>
                                 <tr>
@@ -115,10 +183,21 @@ export default class Cart extends React.Component {
                                         <tr>
                                             <td> {item.article.category.name} </td>
                                             <td> {item.article.name} </td>
-                                            <td > {item.quantity} </td>
+                                            <td > 
+                                                <Form.Control 
+                                                    type="number" 
+                                                    step={1} 
+                                                    min={1} 
+                                                    value={item.quantity} 
+                                                    data-article-id = {item.article.articleId}
+                                                    onChange={(e) => this.updateQuantity(e as any)}/>
+                                            </td>
                                             <td> {Number(item.article.articlePrices[item.article.articlePrices.length - 1].price).toFixed(2)} BAM</td>
                                             <td> {Number(item.quantity * item.article.articlePrices[item.article.articlePrices.length - 1].price).toFixed(2)} BAM</td>
-                                            <td>...</td>
+                                            <td>
+                                                <FontAwesomeIcon icon={faMinusSquare}
+                                                                 onClick={() => this.removeFromCart(item.article.articleId)} />
+                                            </td>
                                         </tr>
                                     )
                                 }, this)}
@@ -134,9 +213,14 @@ export default class Cart extends React.Component {
                                 </tr>
                             </tfoot>
                         </Table>
+                        </>
+                        ) : (
+                            <p>Your cart is empty!</p>
+                        )}
+                        <Alert variant="success" className={this.state.message ? '' : 'd-none'}>{this.state.message}</Alert>
                     </Modal.Body>
                     <Modal.Footer>
-                        <Button variant="primary">
+                        <Button variant="primary" onClick={() => this.makeOrder()} disabled={this.state.cart?.cartArticles.length === 0}>
                             Make an order
                         </Button>
                     </Modal.Footer>
